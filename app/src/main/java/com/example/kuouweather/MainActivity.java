013339a0ca42fbbf627182afd19bc1a0318d666b
PeachLuis <1527725645@qq.com>
2021-04-01 17:48:15 +0800

    更新README中版本

--- app/src/main/java/com/example/kuouweather/MainActivity.java
+++ app/src/main/java/com/example/kuouweather/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
